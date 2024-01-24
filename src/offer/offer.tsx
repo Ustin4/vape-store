6754158af358563990098de7f8fd7e5700828563
Bubchao <mityaborisenok@mail.ru>
2024-01-24 16:21:51 +0300

    gg

--- src/offer/offer.tsx
+++
@@ -1,5 +0,0 @@
-import s from "./offer.module.scss";
-
-export const Offer = () => {
-  return <div className={s.box}>лучшие предложения</div>;
-};
--- src/price/price.tsx
+++
@@ -1,8 +0,0 @@
-import s from "./price.module.scss";
-
-type Props = {
-  price: number;
-};
-export const Price = ({ price }: Props) => {
-  return <div className={s.price}>{price}</div>;
-};
